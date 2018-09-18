2daa5a6c457181d28a864bd285d48e9d95ef0b60
GengYu Rao <zouyoo@outlook.com>
2018-09-18 15:47:05 +0800

    debugging [DONOT BUILD]

--- hbase-client/src/main/java/org/apache/hadoop/hbase/ipc/RdmaNative.java
+++ hbase-client/src/main/java/org/apache/hadoop/hbase/ipc/RdmaNative.java
@@ -10,25 +10,21 @@ public class RdmaNative {
       }
         // This function must be called exactly once to construct necessary structs.
         // It will construct rdmaContext and other global var.
-        public static native boolean rdmaInitGlobal();
+    public native boolean rdmaInitGlobal();
         // This function must be called exactly once to destruct global structs.
-        public static native void rdmaDestroyGlobal();
+    public native void rdmaDestroyGlobal();
     
-        // Connect to remote host. Blocked operation. If success, returnedConn.errorCode holds 0.
-        public static native RdmaClientConnection rdmaConnect(String addr, int port);
+    // Connect to remote host. Blocked operation. If failed, return null.
+    public native RdmaClientConnection rdmaConnect(String addr, int port);
         // This function must be called once by server, to bind a port.
-        public static native boolean rdmaBind(int port);
-        // Wait and accept a connection. Blocked operation. If success, returnedConn.errorCode holds 0.
-        public static native RdmaServerConnection rdmaBlockedAccept();
+    public native boolean rdmaBind(int port);
+    // Wait and accept a connection. Blocked operation. If failed, return null.
+    public native RdmaServerConnection rdmaBlockedAccept();
     
         public class RdmaClientConnection {
             private long ptrCxxClass;
-            private int errorCode;
     
             public native boolean isClosed();
-            public boolean isConnectSucceed() {
-                return errorCode == 0;
-            }
             // returned ByteBuffer MAY be invalidated on next readResponse(). (invalidated if the bytebuffer is created without copying data)
             // return null object if the read failed.
             public native ByteBuffer readResponse(); // blocked. Will wait for the server for response.
@@ -41,6 +37,7 @@ public class RdmaNative {
                 The server holds two buffer. DynamicBufferTokenBuffer holds std::pair<Magic, DynamicBufferToken>, and DynamicBuffer
                 holds the real data. The Magic is inited to 0x00000000 and set to 0xffffffff if DynamicBufferToken is ready to use.
                 (For the initial 4K buffer, the magic is 0x00000000)
+
                 On accepting connection, the server creates a 4K DynamicBuffer, and register it, put the token into DynamicBufferTokenBuffer.
             begin:
                 DynamicBufferTokenBuffer has 3 area: magic, currentQuerySize, and DynamicBufferToken.
@@ -54,20 +51,18 @@ public class RdmaNative {
                 
                 Once the query is wrote into server, the client must set Magic to 0xaaaaaaaa(use compareAndSwap, if possible. It doesn't matter).
                 The server call isQueryReadable() to check if the Magic is 0xaaaaaaaa. 
+
                 After the response is ready, the server use writeResponse to write its data to local buffer. (In fact, the memory in ByteBuffer maybe 
                 used and prevent its GC). The DynamicBufferToken is updated, then the Magic is set to 0x55555555. The client check the value again
                 and again. Once available, the client can get the response.
+
                 I have not consider how should I close the connection. Maybe just destruct the Rdma*Connection, and destruct the insiding C++ QP. 
                 If we want to reuse the connection in the future, just leave the connection there and the server use current DynamicBuffer as initial
                 buffer, goto begin;
             */
             private long ptrCxxClass;
-            private int errorCode;
     
             public native boolean isClosed();
-            public boolean isAcceptSucceed() {
-                return errorCode == 0;
-            }
             public native boolean isQueryReadable();
             // returned ByteBuffer MAY be invalidated on next readQuery()
             // return null object if the read failed.
